6b9f9272dc836e996dfa809f72875d2fe5f74909
Rein4ldoAlv3s <naldos15@hotmail.com>
2022-03-02 17:24:18 -0300

    endpoint categoria funcionam

--- src/main/java/com/reinaldo/resources/CategoriaResource.java
+++ src/main/java/com/reinaldo/resources/CategoriaResource.java
@@ -1,14 +1,22 @@
 package com.reinaldo.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.reinaldo.domain.Categoria;
 import com.reinaldo.dto.CategoriaDTO;
@@ -22,9 +30,32 @@ public class CategoriaResource {
 	private CategoriaService categoriaService;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<?> listar(@PathVariable Integer id){
+	public ResponseEntity<Categoria> buscar(@PathVariable Integer id){
 		Categoria obj = categoriaService.listar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO){
+		Categoria obj = categoriaService.fromDTO(categoriaDTO);
+		obj = categoriaService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO objDto, @PathVariable Integer id ){
+		Categoria obj = categoriaService.fromDTO(objDto);
+		obj.setId(id);
+		obj = categoriaService.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		categoriaService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	@GetMapping
@@ -33,5 +64,6 @@ public class CategoriaResource {
 		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
 	}
+	
 	
 }
--- src/main/java/com/reinaldo/services/CategoriaService.java
+++ src/main/java/com/reinaldo/services/CategoriaService.java
@@ -6,21 +6,48 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.reinaldo.cursomcdois.services.exceptions.DataIntegrityException;
+import com.reinaldo.cursomcdois.services.exceptions.ObjectNotFoundException;
 import com.reinaldo.domain.Categoria;
+import com.reinaldo.dto.CategoriaDTO;
 import com.reinaldo.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
-	private CategoriaRepository categoriaRepository;
+	private CategoriaRepository repo;
 	
 	public Categoria listar(Integer id) {
-		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElse(null);
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 	
 	public List<Categoria> listarTodos(){
-		return categoriaRepository.findAll();
+		return repo.findAll();
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Categoria update(Categoria obj) {
+		listar(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		listar(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos"); 
+		}
 	}
 }
